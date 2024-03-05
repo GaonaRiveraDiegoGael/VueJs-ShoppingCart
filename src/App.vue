06cff4fa89694e6a30cc2633cfd22c57e2ee125d
Diego Gaona <gaonariverad@gmail.com>
2024-03-05 11:54:31 -0600

    ⭐ Feat: App.vue Add a list of items using v-for

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,22 @@ import { ref } from 'vue'
 // de tipo string
 const header = ref('App lista de compras');
 const shoppingIcon = ref('material-icons shopping-cart-icon');
+// Creando una referencia reactiva
+// para alamacenar el valor de la lista
+const items = ref([
+  { id: 0, label: 'Leche' },
+  { id: 1, label: 'Arroz' },
+  { id: 2, label: 'Carne' },
+  { id: 3, label: 'Pan' },
+  { id: 4, label: 'Huevos' }
+]);
 </script>
 
 <template>
   <h1> <i :class="shoppingIcon">local_mall</i> {{ header }}</h1>
+  <ul>
+    <li v-for="({id, label}, i) in items" v-bind:key="id">⭐ {{ label }}</li>
+  </ul>
 </template>
 
 <style scoped>
@@ -17,4 +29,3 @@ const shoppingIcon = ref('material-icons shopping-cart-icon');
   font-size: 2rem;
 }
 </style>
-
