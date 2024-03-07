d97ada9f77aafda5756516fba4370a2d432b55b9
Diego Gaona <gaonariverad@gmail.com>
2024-03-07 12:01:15 -0600

    ⭐FEAT: Cheackboxes Bind cheackboxes to newItemPriority

--- src/App.vue
+++ src/App.vue
@@ -16,11 +16,15 @@ const items = ref([
   { id: 4, label: 'Huevos' }
 ]);
 const newItem = ref('');
+const newItemPriority = ref('low')
 </script>
 
 <template>
   <h1> <i :class="shoppingIcon">local_mall</i> {{ header }}</h1>
   <input v-model="newItem" type="text" placeholder="Agregar Articulo">
+  <label><input type="radio" v-model="newItemPriority" value="low">Baja</label>
+  <label><input type="radio" v-model="newItemPriority" value="high">Alta</label>
+  {{ newItemPriority === 'low'?'🧊':'🔥' }}
   <ul>
     <li v-for="{id, label} in items" v-bind:key="id">🎈 {{ label }}</li>
   </ul>
